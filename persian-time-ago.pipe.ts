37d7b5763e584f813814559834aad27fde994de8
keyone2693 <key.one72@gmail.com>
2019-04-21 20:49:05 +0430

    1.2.0

--- persian-time-ago.pipe.ts
+++ persian-time-ago.pipe.ts
@@ -1,77 +1,77 @@
-import {Pipe, PipeTransform, NgZone, ChangeDetectorRef, OnDestroy} from "@angular/core";
+import { Pipe, PipeTransform, NgZone, ChangeDetectorRef, OnDestroy } from "@angular/core";
 @Pipe({
-	name:'persianTimeAgo',
-	pure:false
+    name: 'persianTimeAgo',
+    pure: false
 })
 export class PersianTimeAgoPipe implements PipeTransform, OnDestroy {
-	private timer: number;
-	constructor(private changeDetectorRef: ChangeDetectorRef, private ngZone: NgZone) {}
-	transform(value:string) {
-		this.removeTimer();
-		let d = new Date(value);
-		let now = new Date();
-		let seconds = Math.round(Math.abs((now.getTime() - d.getTime())/1000));
-		let timeToUpdate = (Number.isNaN(seconds)) ? 1000 : this.getSecondsUntilUpdate(seconds) *1000;
-		this.timer = this.ngZone.runOutsideAngular(() => {
-			if (typeof window !== 'undefined') {
-				return window.setTimeout(() => {
-					this.ngZone.run(() => this.changeDetectorRef.markForCheck());
-				}, timeToUpdate);
-			}
-			return null;
-		});
-		let minutes = Math.round(Math.abs(seconds / 60));
-		let hours = Math.round(Math.abs(minutes / 60));
-		let days = Math.round(Math.abs(hours / 24));
-		let months = Math.round(Math.abs(days/30.416));
-		let years = Math.round(Math.abs(days/365));
-		if (Number.isNaN(seconds)){
-			return '';
-		} else if (seconds <= 45) {
-			return 'چند ثانیه قبل';
-		} else if (seconds <= 90) {
-			return 'دقیقه ای پیش';
-		} else if (minutes <= 45) {
-			return minutes + ' دقیقه پیش';
-		} else if (minutes <= 90) {
-			return 'ساعتی پیش';
-		} else if (hours <= 22) {
-			return hours + ' ساعت پیش';
-		} else if (hours <= 36) {
-			return 'روزی پیش';
-		} else if (days <= 25) {
-			return days + ' روز پیش';
-		} else if (days <= 45) {
-			return 'یک ماه پیش';
-		} else if (days <= 345) {
-			return months + ' ماه پیش';
-		} else if (days <= 545) {
-			return 'یک سال پیش';
-		} else { // (days > 545)
-			return years + ' سال پیش';
-		}
-	}
-	ngOnDestroy(): void {
-		this.removeTimer();
-	}
-	private removeTimer() {
-		if (this.timer) {
-			window.clearTimeout(this.timer);
-			this.timer = null;
-		}
-	}
-	private getSecondsUntilUpdate(seconds:number) {
-		let min = 60;
-		let hr = min * 60;
-		let day = hr * 24;
-		if (seconds < min) { // less than 1 min, update every 2 secs
-			return 2;
-		} else if (seconds < hr) { // less than an hour, update every 30 secs
-			return 30;
-		} else if (seconds < day) { // less then a day, update every 5 mins
-			return 300;
-		} else { // update every hour
-			return 3600;
-		}
-	}
+    private timer: number;
+    constructor(private changeDetectorRef: ChangeDetectorRef, private ngZone: NgZone) { }
+    transform(value: string) {
+        this.removeTimer();
+        const d = new Date(value);
+        const now = new Date();
+        const seconds = Math.round(Math.abs((now.getTime() - d.getTime()) / 1000));
+        const timeToUpdate = (Number.isNaN(seconds)) ? 1000 : this.getSecondsUntilUpdate(seconds) * 1000;
+        this.timer = this.ngZone.runOutsideAngular(() => {
+            if (typeof window !== 'undefined') {
+                return window.setTimeout(() => {
+                    this.ngZone.run(() => this.changeDetectorRef.markForCheck());
+                }, timeToUpdate);
+            }
+            return null;
+        });
+        const minutes = Math.round(Math.abs(seconds / 60));
+        const hours = Math.round(Math.abs(minutes / 60));
+        const days = Math.round(Math.abs(hours / 24));
+        const months = Math.round(Math.abs(days / 30.416));
+        const years = Math.round(Math.abs(days / 365));
+        if (Number.isNaN(seconds)) {
+            return '';
+        } else if (seconds <= 45) {
+            return 'چند ثانیه قبل';
+        } else if (seconds <= 90) {
+            return 'دقیقه ای پیش';
+        } else if (minutes <= 45) {
+            return minutes + ' دقیقه پیش';
+        } else if (minutes <= 90) {
+            return 'ساعتی پیش';
+        } else if (hours <= 22) {
+            return hours + ' ساعت پیش';
+        } else if (hours <= 36) {
+            return 'روزی پیش';
+        } else if (days <= 25) {
+            return days + ' روز پیش';
+        } else if (days <= 45) {
+            return 'یک ماه پیش';
+        } else if (days <= 345) {
+            return months + ' ماه پیش';
+        } else if (days <= 545) {
+            return 'یک سال پیش';
+        } else { // (days > 545)
+            return years + ' سال پیش';
+        }
+    }
+    ngOnDestroy(): void {
+        this.removeTimer();
+    }
+    private removeTimer() {
+        if (this.timer) {
+            window.clearTimeout(this.timer);
+            this.timer = null;
+        }
+    }
+    private getSecondsUntilUpdate(seconds: number) {
+        const min = 60;
+        const hr = min * 60;
+        const day = hr * 24;
+        if (seconds < min) { // less than 1 min, update every 2 secs
+            return 2;
+        } else if (seconds < hr) { // less than an hour, update every 30 secs
+            return 30;
+        } else if (seconds < day) { // less then a day, update every 5 mins
+            return 300;
+        } else { // update every hour
+            return 3600;
+        }
+    }
 }
--- test/persian-time-ago.pipe.spec.ts
+++ test/persian-time-ago.pipe.spec.ts
@@ -1,10 +1,10 @@
 var expect = require('chai').expect;
 var sinon = require('sinon');
-import {NgZone} from '@angular/core';
-import {PersianTimeAgoPipe} from "../persian-time-ago.pipe";
+import { NgZone } from '@angular/core';
+import { PersianTimeAgoPipe } from "../persian-time-ago.pipe";
 
 class NgZoneMock {
-	runOutsideAngular (fn: Function) {
+	runOutsideAngular(fn: Function) {
 		return fn();
 	}
 	run(fn: Function) {
@@ -13,8 +13,8 @@ class NgZoneMock {
 };
 
 describe('persian-time-ago-pipe', () => {
-	const now:Date = new Date();
-	var clock:any;
+	const now: Date = new Date();
+	var clock: any;
 	const oneSec = 1000;
 	const oneMin = oneSec * 60;
 	const oneHour = oneMin * 60;
@@ -30,7 +30,7 @@ describe('persian-time-ago-pipe', () => {
 		let pipe = new PersianTimeAgoPipe(null, new NgZoneMock() as NgZone);
 		it('\'چند ثانیه قبل\' tests', () => {
 			var pastDate = new Date();
-			for (let i =0; i < 45; i++){
+			for (let i = 0; i < 45; i++) {
 				clock.tick(oneSec);
 				if (i < 44) {
 					expect(pipe.transform(pastDate.toString())).to.equal('چند ثانیه قبل');
@@ -40,9 +40,9 @@ describe('persian-time-ago-pipe', () => {
 		it('\'دقیقه ای پیش\' tests', () => {
 			var pastDate = new Date();
 			clock.tick(oneSec * 45);
-			for (let i =45; i < 89; i++){
+			for (let i = 45; i < 89; i++) {
 				clock.tick(oneSec);
-				if (i < 89){
+				if (i < 89) {
 					expect(pipe.transform(pastDate.toString())).to.equal('دقیقه ای پیش');
 				} else {
 					expect(pipe.transform(pastDate.toString())).not.to.equal('دقیقه ای پیش');
@@ -52,12 +52,12 @@ describe('persian-time-ago-pipe', () => {
 		it('\'x دقیقه پیش\' tests', () => {
 			var pastDate = new Date();
 			clock.tick(oneSec * 50);
-			for (let i =1; i < 44; i++){
+			for (let i = 1; i < 44; i++) {
 				clock.tick(oneMin);
-				if (i < 44){
-					expect(pipe.transform(pastDate.toString())).to.equal(i+1 + ' دقیقه پیش');
+				if (i < 44) {
+					expect(pipe.transform(pastDate.toString())).to.equal(i + 1 + ' دقیقه پیش');
 				} else {
-					expect(pipe.transform(pastDate.toString())).not.to.equal(i+1 + ' دقیقه پیش');
+					expect(pipe.transform(pastDate.toString())).not.to.equal(i + 1 + ' دقیقه پیش');
 				}
 			}
 		});
@@ -65,9 +65,9 @@ describe('persian-time-ago-pipe', () => {
 			var pastDate = new Date();
 			//set the time forward 45 mins
 			clock.tick(oneMin * 45);
-			for (let i =45; i < 120; i++){
+			for (let i = 45; i < 120; i++) {
 				clock.tick(oneMin);
-				if (i < 90){
+				if (i < 90) {
 					expect(pipe.transform(pastDate.toString())).to.equal('ساعتی پیش');
 				} else {
 					expect(pipe.transform(pastDate.toString())).not.to.equal('ساعتی پیش');
@@ -78,21 +78,21 @@ describe('persian-time-ago-pipe', () => {
 			var pastDate = new Date();
 			//set the time forward 50 mins
 			clock.tick(oneMin * 50);
-			for (let i = 1; i < 25; i++){
+			for (let i = 1; i < 25; i++) {
 				clock.tick(oneHour);
-				if (i < 22){
-					expect(pipe.transform(pastDate.toString())).to.equal(i+1 + ' ساعت پیش');
+				if (i < 22) {
+					expect(pipe.transform(pastDate.toString())).to.equal(i + 1 + ' ساعت پیش');
 				} else {
-					expect(pipe.transform(pastDate.toString())).not.to.equal(i+1 + ' ساعت پیش');
+					expect(pipe.transform(pastDate.toString())).not.to.equal(i + 1 + ' ساعت پیش');
 				}
 			}
 		});
 		it('\'روزی پیش\' tests', () => {
 			var pastDate = new Date();
 			clock.tick(oneHour * 22);
-			for (let i = 22; i < 40; i++){
+			for (let i = 22; i < 40; i++) {
 				clock.tick(oneHour);
-				if (i < 36){
+				if (i < 36) {
 					expect(pipe.transform(pastDate.toString())).to.equal('روزی پیش');
 				} else {
 					expect(pipe.transform(pastDate.toString())).not.to.equal('روزی پیش');
@@ -102,21 +102,21 @@ describe('persian-time-ago-pipe', () => {
 		it('\'x روز پیش\' tests', () => {
 			var pastDate = new Date();
 			clock.tick(oneHour * 35);
-			for (let i = 1; i < 30; i++){
+			for (let i = 1; i < 30; i++) {
 				clock.tick(oneDay);
-				if (i < 25){
-					expect(pipe.transform(pastDate.toString())).to.equal(i+1 + ' روز پیش');
+				if (i < 25) {
+					expect(pipe.transform(pastDate.toString())).to.equal(i + 1 + ' روز پیش');
 				} else {
-					expect(pipe.transform(pastDate.toString())).not.to.equal(i+1 + ' روز پیش');
+					expect(pipe.transform(pastDate.toString())).not.to.equal(i + 1 + ' روز پیش');
 				}
 			}
 		});
 		it('\'a یک ماه پیش\' tests', () => {
 			var pastDate = new Date();
 			clock.tick(oneDay * 25);
-			for (let i = 25; i < 50; i++){
+			for (let i = 25; i < 50; i++) {
 				clock.tick(oneDay);
-				if (i < 45){
+				if (i < 45) {
 					expect(pipe.transform(pastDate.toString())).to.equal('یک ماه پیش');
 				} else {
 					expect(pipe.transform(pastDate.toString())).not.to.equal('یک ماه پیش');
@@ -126,21 +126,21 @@ describe('persian-time-ago-pipe', () => {
 		it('\'x ماه پیش\' tests', () => {
 			var pastDate = new Date();
 			clock.tick(oneDay * 43);
-			for (let i = 1; i < 13; i++){
+			for (let i = 1; i < 13; i++) {
 				clock.tick(oneMonth);
-				if (i < 10){
-					expect(pipe.transform(pastDate.toString())).to.equal(i +1 + ' ماه پیش');
+				if (i < 10) {
+					expect(pipe.transform(pastDate.toString())).to.equal(i + 1 + ' ماه پیش');
 				} else {
-					expect(pipe.transform(pastDate.toString())).not.to.equal(i +1 + ' ماه پیش');
+					expect(pipe.transform(pastDate.toString())).not.to.equal(i + 1 + ' ماه پیش');
 				}
 			}
 		});
 		it('\'یک سال پیش\' tests', () => {
 			var pastDate = new Date();
 			clock.tick(oneDay * 345);
-			for (let i = 345; i < 545; i++){
+			for (let i = 345; i < 545; i++) {
 				clock.tick(oneDay);
-				if (i < 545){
+				if (i < 545) {
 					expect(pipe.transform(pastDate.toString())).to.equal('یک سال پیش');
 				} else {
 					expect(pipe.transform(pastDate.toString())).not.to.equal('یک سال پیش');
